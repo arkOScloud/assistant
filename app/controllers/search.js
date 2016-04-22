0c93bc3bef0b3768fc7adb0b6ff732f7116a0a0d
Jacob Cook <jacob@peakwinter.net>
2016-04-22 15:02:24 -0400

    fix search

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,6 +1,16 @@
+/* global requireNode */
 import Ember from 'ember';
+
+const ipc = requireNode('electron').ipcRenderer;
+
 
 export default Ember.Controller.extend({
   isSearching: true,
-  servers: Ember.A()
+  servers: Ember.A(),
+
+  actions: {
+    openGenesis: function(addr) {
+      ipc.send('openGenesis', addr);
+    }
+  }
 });
